2b98fb1247ee61f0c4d6132d3c21212497729349
Dvir Ben Asuli <66518359+DBenAsuli@users.noreply.github.com>
2024-06-10 22:15:59 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Advanced Natural Language Processing     Exercise 1
+# Dvir Ben Asuli                           318208816
+# The Hebrew University of Jerusalem       June 2024
+
+if __name__ == '__main__':
+    question = input("Please enter question number: ")
+
+    if (question == 1):
+      pass
+    else if (question == 2):
+      pass
+    else if (question == 3):
+      pass
+    else:
+      print("Invalid Question Number (should be 1, 2 or 3)")
